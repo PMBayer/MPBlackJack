8949facfcbd86a0fbc450509b0f2e15cde419ab0
Pascal Michael Bayer <77111184+PMBayer@users.noreply.github.com>
2021-03-07 14:36:25 +0100

    commit

--- static/BlackJack.js
+++ static/BlackJack.js
@@ -18,7 +18,7 @@ function Card(zahl, zeichen2) {
     }
     this.color = zeichen[zeichen2];
     this.getCard = function () {
-        karte1 = this.number + ' ' + this.color + ', ';
+        const karte1 = this.number + ' ' + this.color + ', ';
         return karte1;
     }
     this.wert = function () {
@@ -36,23 +36,23 @@ function Card(zahl, zeichen2) {
 function Ziehstapel() {
     this.stapel = [];
     this.erneuern = function () {
-        var anzahldecks = 6;
-        for (i = 1; i < 14; i++) {
-            for (i2 = 0; i2 < 4; i2++) {
-                for (i3 = 0; i3 < anzahldecks; i3++) {
-                    var card5 = new Card(i, i2);
+        const anzahldecks = 6;
+        for (let i = 1; i < 14; i++) {
+            for (let i2 = 0; i2 < 4; i2++) {
+                for (let i3 = 0; i3 < anzahldecks; i3++) {
+                    const card5 = new Card(i, i2);
                     this.stapel.push(card5);
                 }
             }
         }
     }
     this.lang = function () {
-        var länge = this.stapel.length;
-        return länge;
+        const laenge = this.stapel.length;
+        return laenge;
     }
     this.geben = function () {
-        var stelle = getRandomInt(this.stapel.length);
-        var karte = this.stapel.splice(stelle, 1);
+        const stelle = getRandomInt(this.stapel.length);
+        const karte = this.stapel.splice(stelle, 1);
         return karte[0];
     }
     this.leer = function () {
@@ -69,35 +69,35 @@ Ziehstapel1.erneuern();
 function Hand(s) {
     this.blatt = [];
     this.ziehen = function () {
-        var hilf3 = Ziehstapel1.geben();
+        const hilf3 = Ziehstapel1.geben();
         this.blatt.push(hilf3);
-        var mischen = s.leer();
+        const mischen = s.leer();
         if (mischen) {
             s.erneuern();
         }
     };
     this.zeigen = function (a) {
-        var c = this.blatt[a];
+        const c = this.blatt[a];
         //var string1=c.getCard();
         return c;//string1;
     }
     this.volleHand = function () {
-        var string2 = '';
-        for (var i = 0; i < this.blatt.length; i++) {
-            var c2 = this.blatt[i];
+        let string2 = '';
+        for (let i = 0; i < this.blatt.length; i++) {
+            const c2 = this.blatt[i];
             string2 = string2.concat(c2.getCard());
         }
         return string2;
     }
     this.gezogen = function () {
-        var c3 = this.blatt[this.blatt.length - 1];
+        const c3 = this.blatt[this.blatt.length - 1];
         //string3=c3.getCard();
         return c3; //string3;
     }
     this.Handwert = function () {
-        var total = 0;
-        for (h1 = 0; h1 < this.blatt.length; h1++) {
-            var kartenwert = this.blatt[h1];
+        let total = 0;
+        for (let h1 = 0; h1 < this.blatt.length; h1++) {
+            const kartenwert = this.blatt[h1];
             total += kartenwert.wert();
         }
         return total;
@@ -120,10 +120,10 @@ function dealer(stapel1) {
     }
     this.Drettungmöglich = function () {
         if (this.dHand.Handwert() > 21) {
-            var anzahl = this.dHand.Groß();
-            for (ret1 = 0; ret1 < anzahl; ret1++) {
-                var Kriterium1 = this.dHand.zeigen(ret1);
-                var Kriterium2 = Kriterium1.number;
+            const anzahl = this.dHand.Groß();
+            for (let ret1 = 0; ret1 < anzahl; ret1++) {
+                const Kriterium1 = this.dHand.zeigen(ret1);
+                const Kriterium2 = Kriterium1.number;
                 if (Kriterium2 == 'Ass') {
                     this.veringern = true;
                     Kriterium1.number = 1;
@@ -133,10 +133,8 @@ function dealer(stapel1) {
         }
     }
     this.Dverloren = function () {
-        if (this.dHand.Handwert() < 22) {
-            return false;
-        }
-        return true;
+        return this.dHand.Handwert() >= 22;
+
     }
     this.getDhand = function () {
         return this.dHand;
@@ -146,13 +144,13 @@ function dealer(stapel1) {
 function spieler(stapel1) {
     this.sHand = new Hand(stapel1);
     this.Sstart = function () {
-        for (d1 = 0; d1 < 2; d1++) {
+        for (let d1 = 0; d1 < 2; d1++) {
             this.sHand.ziehen();
         }
     }
     this.Sspiel = function () {
+        const spielen = false;//window.prompt('wollen sie eine Karte ziehen?', 'true oder false');
         do {
-            var spielen = false;//window.prompt('wollen sie eine Karte ziehen?', 'true oder false');
             if (spielen) {
                 this.sHand.ziehen();
                 this.Srettungmöglich();
@@ -161,10 +159,10 @@ function spieler(stapel1) {
     }
     this.Srettungmöglich = function () {
         if (this.sHand.Handwert() > 21) {
-            var anzahl = this.sHand.Groß();
-            for (ret1 = 0; ret1 < anzahl; ret1++) {
-                var Kriterium1 = this.sHand.zeigen(ret1);
-                var Kriterium2 = Kriterium1.number;
+            const anzahl = this.sHand.Groß();
+            for (let ret1 = 0; ret1 < anzahl; ret1++) {
+                const Kriterium1 = this.sHand.zeigen(ret1);
+                const Kriterium2 = Kriterium1.number;
                 if (Kriterium2 == 'Ass') {
                     Kriterium1.number = 1;
                     ret1 = anzahl;
@@ -188,7 +186,7 @@ function Blackjack(mitspielen, liste) {
     this.Spielerliste = []
     this.Vorbereiten = function () {
         this.Dealer1.Dstart();
-        for (Spielermenge = 0; Spielermenge < mitspielen.length; Spielermenge++) {
+        for (let Spielermenge = 0; Spielermenge < mitspielen.length; Spielermenge++) {
             if (mitspielen[Spielermenge]) {
                 var Spieler1 = new spieler(liste);
                 Spieler1.Sstart();
@@ -197,16 +195,16 @@ function Blackjack(mitspielen, liste) {
         }
     }
     this.Spielen = function () {
-        for (Spielermenge2 = 0; Spielermenge2 < this.Spielerliste.length; Spielermenge2++) {
-            var hilfe = this.Spielerliste[Spielermenge2];
+        for (let Spielermenge2 = 0; Spielermenge2 < this.Spielerliste.length; Spielermenge2++) {
+            const hilfe = this.Spielerliste[Spielermenge2];
             hilfe.Sspiel();
         }
         this.Dealer1.Dspiel();
     }
     this.anzeigen = function () {
-        for (Spielermenge2 = 0; Spielermenge2 < this.Spielerliste.length; Spielermenge2++) {
-            var hilfe2 = this.Spielerliste[Spielermenge2];
-            var hilfe3 = hilfe2.getShand();
+        for (let Spielermenge2 = 0; Spielermenge2 < this.Spielerliste.length; Spielermenge2++) {
+            const hilfe2 = this.Spielerliste[Spielermenge2];
+            const hilfe3 = hilfe2.getShand();
             console.log(hilfe3);
         }
         var hilfe4 = this.Dealer1.getDhand();
@@ -225,10 +223,10 @@ function Blackjack(mitspielen, liste) {
         return 'gewonnen'
     }
     this.Resultat = function () {
-        var Ergebnis = '';
-        for (Spielermenge3 = 0; Spielermenge3 < this.Spielerliste.length; Spielermenge3++) {
-            var hilfe8 = this.verloren(this.Spielerliste[Spielermenge3]);
-            var zwischenspeicher = 'Spieler' + Spielermenge3 + ' hat ' + hilfe8 + ',';
+        let Ergebnis = '';
+        for (let Spielermenge3 = 0; Spielermenge3 < this.Spielerliste.length; Spielermenge3++) {
+            const hilfe8 = this.verloren(this.Spielerliste[Spielermenge3]);
+            const zwischenspeicher = 'Spieler' + Spielermenge3 + ' hat ' + hilfe8 + ',';
             Ergebnis = Ergebnis.concat(zwischenspeicher);
         }
         console.log(Ergebnis);
